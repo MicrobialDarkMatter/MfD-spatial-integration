67ebf54b93e6b95249fed6412794a6a9b667ab61
Mads Corfixen <maco@cs.aau.dk>
2024-05-22 14:24:50 +0200

    update to query function

--- SPARQLExtensions/queryGraph.py
+++ SPARQLExtensions/queryGraph.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 import pandas as pd
 from sshtunnel import SSHTunnelForwarder
@@ -17,20 +18,16 @@ def send_query(query: str):
 
     sparql = SPARQLWrapper(endpoint_url)
 
-    # Selects the variables in the query and stores them in the list_of_variables
-    selection_pattern = r"SELECT(?:\s+\w+)*\s+((?:\?\w+\s*)+)"
-    selection_match = re.search(selection_pattern, query)
-    selection_string = selection_match.group(1).replace("?", "").replace("\n", "")
-    list_of_variables = selection_string.split()
-
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
+    results = sparql.query().convert()  # JSON object
 
+    variables = results["head"]["vars"]
     data = results["results"]["bindings"]
+
     df = pd.DataFrame({
-        var: [result[var]['value'] for result in data] 
-        for var in list_of_variables
+        var: [result.get(var, {'value': None})['value'] for result in data]
+        for var in variables
     })
 
     return df
@@ -40,34 +37,12 @@ if __name__ == "__main__":
     SERVER.start()
 
     query = """
-    PREFIX kwg-ont: <http://stko-kwg.geog.ucsb.edu/lod/ontology#>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX mfd: <http://purl.archive.org/domain/mfd#>
-    PREFIX oboe: <http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#>
-    PREFIX empo: <http://earthmicrobiome.org/protocols-and-standards/empo/>
-    PREFIX schema: <http://schema.org/>
-    PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-    PREFIX wgs84_pos: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-    PREFIX time: <http://www.w3.org/2006/time#>
-    PREFIX prov: <http://www.w3.org/ns/prov#>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-   
-    SELECT ?rasterCell ?value ?measurementType (COUNT(?site) as ?nSites)
-    WHERE {
-        ?rasterCell a mfd:RasterCell .
-        ?rasterCell oboe:hasMeasurement ?measurement .
-        ?measurement oboe:hasValue ?value ;
-                     oboe:containsMeasurementsOfType ?measurementType .
-        OPTIONAL {
-            ?rasterCell geo:ehCovers ?s2Cell .
-            ?s2Cell a kwg-ont:S2Cell .
-            ?site geo:ehCoveredBy ?s2Cell .
-        }
+    SELECT ?s ?p ?o WHERE {
+        ?s ?p ?o
     }
-    GROUP BY ?rasterCell ?value ?measurementType
+    LIMIT 10
     """
-    
+
     result = send_query(query)
     print(result)
 
